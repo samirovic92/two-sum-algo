861d11f1fee5708537c07d372a1efc2ad27b24bd
samirovic92 <s.elidrissi1992@gmail.com>
2021-10-18 15:26:00 +0200

    Refacto

--- src/test/java/TwoSumAlgorithmTest.java
+++ src/test/java/TwoSumAlgorithmTest.java
@@ -5,24 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumAlgorithmTest {
 
-    /*
-    case 1:
-    Input: nums = [2,7,11,15], target = 9
-        Output: [0,1]
-        Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-    case 2:
-        Input: nums = [3,2,4], target = 6
-        Output: [1,2]
-    case 3:
-        Input: nums = [3,3], target = 6
-        Output: [0,1]
-    Constraints:
-        2 <= nums.length <= 104
-        -109 <= nums[i] <= 109
-        -109 <= target <= 109
-        Only one valid answer exists.
-     */
-
     TwoSumAlgorithm twoSumAlgorithm;
 
     @BeforeEach
